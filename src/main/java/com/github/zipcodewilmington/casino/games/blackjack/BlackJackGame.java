fe9e83a60b82d8046a44e5f1ea3c661a4a96fbd3
Troy2797B <112650105+Troy2797B@users.noreply.github.com>
2022-11-16 13:45:40 -0500

    removed implementations

--- src/main/java/com/github/zipcodewilmington/casino/games/blackjack/BlackJackGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/blackjack/BlackJackGame.java
@@ -2,5 +2,5 @@ package com.github.zipcodewilmington.casino.games.blackjack;
 
 import com.github.zipcodewilmington.casino.GameInterface;
 
-public class BlackJackGame implements GameInterface {
+public class BlackJackGame {
 }
