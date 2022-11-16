98947bcfdbc458ca48de61367473f2c3cc1c1b3f
Deepthi Ganji <115901667+deepthireddyg@users.noreply.github.com>
2022-11-16 14:16:01 -0500

    added test classes for GoFish and warGame

--- src/main/java/com/github/zipcodewilmington/casino/games/roulette/RouletteGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/roulette/RouletteGame.java
@@ -2,5 +2,5 @@ package com.github.zipcodewilmington.casino.games.roulette;
 
 import com.github.zipcodewilmington.casino.GameInterface;
 
-public class RouletteGame implements GameInterface {
+public class RouletteGame {
 }
---
+++ src/test/java/com/github/zipcodewilmington/casino/games/GoFish/GoFishPlayerTest.java
@@ -0,0 +1,17 @@
+package com.github.zipcodewilmington.casino.games.GoFish;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GoFishPlayerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
