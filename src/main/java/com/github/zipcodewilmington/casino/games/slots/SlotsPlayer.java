b330f12d50ff2bfa9cdeefc0b14777554522302f
Prathibha <prathibha@Zipcoders-MacBook-Pro-8.local>
2022-11-17 15:33:35 -0500

    Updated slot player and slot game

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsPlayer.java
@@ -4,20 +4,62 @@ import com.github.zipcodewilmington.casino.CasinoAccount;
 import com.github.zipcodewilmington.casino.CasinoAccountManager;
 import com.github.zipcodewilmington.casino.GameInterface;
 import com.github.zipcodewilmington.casino.PlayerInterface;
+import com.github.zipcodewilmington.utils.AnsiColor;
+import com.github.zipcodewilmington.utils.IOConsole;
 
 /**
  * Created by leon on 7/21/2020.
  */
 public class SlotsPlayer implements PlayerInterface {
-    SlotsGame objSlotGame=new SlotsGame();
-    private int winAmount=0;
+//    CasinoAccount objSlotPlayer;
+    private String _slotPlayerName;
+    private String _slotPlayerPassword;
+
+    public String get_slotPlayerName() {
+        return _slotPlayerName;
+    }
+
+    public void set_slotPlayerName(String _slotPlayerName) {
+        this._slotPlayerName = _slotPlayerName;
+    }
+
+    public String get_slotPlayerPassword() {
+        return _slotPlayerPassword;
+    }
+
+    public void set_slotPlayerPassword(String _slotPlayerPassword) {
+        this._slotPlayerPassword = _slotPlayerPassword;
+    }
+
+    public int get_slotPlayerBalance() {
+        return _slotPlayerBalance;
+    }
+
+    public void set_slotPlayerBalance(int _slotPlayerBalance) {
+        this._slotPlayerBalance = _slotPlayerBalance;
+    }
+
+    private int _slotPlayerBalance;
+
+//    private int winAmount=0;
+    private final IOConsole console = new IOConsole(AnsiColor.GREEN);
+
+    public SlotsPlayer() {
+        CasinoAccount objSlotPlayer=getArcadeAccount();
+       this._slotPlayerName=objSlotPlayer.get_name();
+        this._slotPlayerPassword=objSlotPlayer.get_password();
+        this._slotPlayerBalance=objSlotPlayer.get_balance();
+        run();
+    }
+
 
     @Override
     public CasinoAccount getArcadeAccount() {
-        //CasinoAccountManager objmgr=new CasinoAccountManager();
-        //CasinoAccount ObjCasinoAccount=objmgr.getAccount(name,password);
-
-        return null;
+        String name=console.getStringInput("Helloo Slot player!It cost 1$ for each round. Please enter your name ");
+        String password=console.getStringInput("Please enter your password ");
+        CasinoAccountManager objmgr=new CasinoAccountManager();
+        CasinoAccount ObjCasinoAccount=new CasinoAccount(name,password);
+        return ObjCasinoAccount;
     }
 
     @Override
@@ -25,13 +67,14 @@ public class SlotsPlayer implements PlayerInterface {
         return null;
     }
     public void run() {
-    int balance=50;
-        System.out.println("Would you like to play Slots! Y ? N?");
-        String userInput=System.console().readLine();
+      //  SlotsGame objSlotGame=new SlotsGame();
 
-        if(userInput=="Y") objSlotGame.run();
-        if(winAmount!=0) System.out.println("Hey You won "+winAmount+" !");
-        else System.out.println("Better luck next time");
+//    int balance=50;
+//        String userInput=console.getStringInput("Would you like to play Slots! Y ? N?");
+//
+//        if(userInput=="Y") objSlotGame.run();
+//        if(winAmount!=0) System.out.println("Hey You won "+winAmount+" !");
+//        else System.out.println("Better luck next time");
     }
 
 }
--- src/test/java/com/github/zipcodewilmington/casino/games/slots/SlotsPlayerTest.java
+++ src/test/java/com/github/zipcodewilmington/casino/games/slots/SlotsPlayerTest.java
@@ -1,6 +1,8 @@
 package com.github.zipcodewilmington.casino.games.slots;
 
 import com.github.zipcodewilmington.casino.CasinoAccount;
+import com.github.zipcodewilmington.casino.CasinoAccountManager;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,8 +10,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class SlotsPlayerTest {
 
     @Test
-    void getArcadeAccount() {
-       CasinoAccount account=new CasinoAccount("name","password");
+    void testGetArcadeAccount() {
+        String expectedName="name";
+        String expectedPassword="passowrd";
+        Integer expectedbalance=100;
+        CasinoAccount account=new CasinoAccount(expectedName,expectedPassword);
+        //When
+        String actualName=account.get_name();
+        String actualPassword=account.get_password();
+        Integer actualBalanace=account.get_balance();
+        //Then
+        Assert.assertEquals(expectedName,actualName);
+        Assert.assertEquals(expectedPassword,actualPassword);
+        Assert.assertEquals(expectedbalance,actualBalanace);
 
     }
 
