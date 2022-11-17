924273fd2dc100138952228567a2e39a1f105508
AndrewAscone <andrew.ascone@gmail.com>
2022-11-17 08:33:58 -0500

    Made test for creating deck

--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,12 +1,29 @@
 import com.github.zipcodewilmington.casino.Card;
 import com.github.zipcodewilmington.casino.Deck;
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class DeckTest {
 
     @Test
-    public void constructDeck(){
+    public void createDeckTest(){
+        //given: a deck will hold card objects
+        ArrayList<Card> test = new ArrayList<>();
+        //when: the deck is constructed
+        for(Card.Suits suit : Card.Suits.values()){
+            for(int i = 1; i <= 13; i++){
+                //System.out.println(suit);
+                test.add(new Card(i, suit));
+                System.out.println("Card added: " + i + " of " + suit);
+            }
+        }
+        //then: the deck contains 52 cards
+        int expected = 52;
+        int actual = test.size();
+
+        Assert.assertEquals(expected, actual);
     }
 }
